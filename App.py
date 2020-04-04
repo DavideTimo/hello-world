1d7dfd814652ee9bf9b0f4e88bace2b1cd2757e6
DavideTimo <38298738+DavideTimo@users.noreply.github.com>
2020-04-04 02:30:30 +0200

    Commit test

--- App.py
+++ App.py
@@ -2,7 +2,7 @@ import sys
 from PyQt5.QtWidgets import QApplication
 from MainWindow import finestraPrincipale
 from Widget01 import finestraPiccola04
-
+#test
 #anche qui un commentino ci sta
 if __name__ == '__main__':
     app = QApplication(sys.argv)
